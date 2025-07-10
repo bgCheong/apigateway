283d55d8aeab0fe519cbd9c9fe01e3d057c964fa
jbg <jbg4354@naver.com>
2025-07-11 08:51:56 +0900

    first_commit

---
+++ src/main/java/com/example/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.example.apigateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.example.apigateway.jwt.JwtUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final JwtUtil jwtUtil;
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
+
+    public AuthenticationFilter(JwtUtil jwtUtil) {
+        super(Config.class);
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            // 1. 헤더에 Authorization 토큰이 있는지 확인
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            // 2. 토큰을 추출하고 유효성 검사
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String token = authorizationHeader.replace("Bearer ", "");
+
+            if (!jwtUtil.validateToken(token)) {
+                return onError(exchange, "JWT Token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+            
+            logger.info("진입 {}" , request.getPath());
+
+            // 3. 토큰이 유효하면, 헤더에 사용자 정보 추가 후 다음 필터로 전달
+            String id = jwtUtil.getIdFromToken(token);
+            
+            logger.info("id :  {}" , id);
+            ServerHttpRequest newRequest = request.mutate().header("X-User-Id", id).build();
+            
+            logger.info("헤더세팅");
+
+            return chain.filter(exchange.mutate().request(newRequest).build());
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+        return response.setComplete();
+    }
+
+    public static class Config {
+        // Put configuration properties here
+    }
+}
---
+++ src/main/java/com/example/apigateway/jwt/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.example.apigateway.jwt;
+
+import java.security.Key;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // 토큰 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            // MalformedJwtException, ExpiredJwtException 등
+            return false;
+        }
+    }
+
+    // 토큰에서 이메일 정보 추출
+    public String getIdFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+}
