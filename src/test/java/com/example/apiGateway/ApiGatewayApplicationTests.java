8063099877e97153344b42dd0b8c45abd525d0c7
jbg4354 <jbg4354@naver.com>
2025-07-24 16:49:57 +0900

    update docker

--- src/test/java/com/example/apiGateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/example/apiGateway/ApiGatewayApplicationTests.java
@@ -1,18 +1,18 @@
 package com.example.apiGateway;
 
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import com.example.apigateway.jwt.JwtUtil;
-//
-//@SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.example.apigateway.jwt.JwtUtil;
+
+@SpringBootTest
 class ApiGatewayApplicationTests {
 	
-//	
-//	@MockBean
-//	private JwtUtil jwtUtil; 
-//	
+	
+	@MockBean
+	private JwtUtil jwtUtil; 
+	
 	@Test
 	void contextLoads() {
 	}
