cf42fe6a71278ba4d14c78b58692f9857d2ab1e0
jbg4354 <jbg4354@naver.com>
2025-07-24 16:14:39 +0900

    update docker

--- src/test/java/com/example/apiGateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/example/apiGateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,19 @@ package com.example.apiGateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.example.apigateway.jwt.JwtUtil;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	 @MockBean
+	 private JwtUtil jwtUtil; 
+
+    @Test
+    void contextLoads() {
+        // 이 테스트는 이제 컨텍스트를 성공적으로 불러올 수 있습니다.
+    }
 
 }
